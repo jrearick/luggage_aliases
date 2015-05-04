f7c887c7a54eb79c7c006ebf7e309e9c2ec730cc
rlfrahm <frahmryan@gmail.com>
2015-05-04 15:34:25 -0500

    Authors now show up on nodes, author names are managed on profiles

---
+++ templates/luggage_aliases_default.tpl.php
@@ -0,0 +1,9 @@
+<a href="<?php print $GLOBALS['base_url']; ?>/node/<?php print $url; ?>" class="media">
+    <div class="media-left">
+        <img src="<?php print $image_src; ?>">
+    </div>
+    <div class="media-body">
+        <h4 class="media-heading"><?php print $firstname; ?> <?php print $lastname; ?> <small><em><?php print $position; ?></em></small></h4>
+        <small><?php print $bio; ?></small>
+    </div>
+</a>
