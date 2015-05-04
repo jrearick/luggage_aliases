f7a1efc0b3d9c7128f24d9c6a7e265e17b4aff0b
rlfrahm <frahmryan@gmail.com>
2015-05-04 16:21:56 -0500

    Code for aliases widget

--- templates/luggage_aliases_default.tpl.php
+++ templates/luggage_aliases_default.tpl.php
@@ -1,4 +1,5 @@
-<a href="<?php print $GLOBALS['base_url']; ?>/node/<?php print $url; ?>" class="media">
+<a href="<?php print $GLOBALS['base_url']; ?><?php if(!$aliasName) print '/node/' . $url; else print $url; ?>" class="media">
+    <?php if(!$aliasName): ?>
     <div class="media-left">
         <img src="<?php print $image_src; ?>">
     </div>
@@ -6,4 +7,7 @@
         <h4 class="media-heading"><?php print $firstname; ?> <?php print $lastname; ?> <small><em><?php print $position; ?></em></small></h4>
         <small><?php print $bio; ?></small>
     </div>
+    <?php else: ?>
+      <h4 class="media-heading"><?php print $aliasName; ?></h4>
+    <?php endif; ?>
 </a>
